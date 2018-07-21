3e109179dbe441883167474e547e423a754b9097
Archintyeron <35148163+Archintyeron@users.noreply.github.com>
2018-07-21 14:38:47 -0300

    Tests creation

--- java/subnav/src/test/java/br/com/buscape/subnav/SubnavApplicationTests.java
+++ java/subnav/src/test/java/br/com/buscape/subnav/SubnavApplicationTests.java
@@ -1,16 +1,48 @@
 package br.com.buscape.subnav;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.buscape.subnav.web.MoveSubService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SubnavApplicationTests {
 
+	@Autowired
+	private MoveSubService moveSubService;
+
 	@Test
-	public void contextLoads() {
+	public void testInvalidCommand() {
+
+		String actions = "InvalidAction";
+		String returns = moveSubService.moveSub(actions);
+
+		assertEquals("Invalid Command", returns);
+	}
+
+	@Test
+	public void testUpperLimit() {
+
+		String actions = "UUU";
+		String returns = moveSubService.moveSub(actions);
+
+		String[] coords = returns.split(" ");
+		assertEquals(Integer.valueOf(0), Integer.valueOf(coords[2]));
+	}
+
+	@Test
+	public void testMoveSub() {
+
+		String actions = "RMMLMMMDDLL";
+		String returns = moveSubService.moveSub(actions);
+
+		assertEquals("2 3 -2 SUL", returns);
 	}
 
 }
